e3c46bd2c9dfec11d09191d335a870cabfc33878
Kevin Tang <kevin@Kevins-MacBook-Pro.local>
2017-11-25 22:58:36 -0700

    Hooking up api to client

--- service/index.js
+++ service/index.js
@@ -1,20 +1,29 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import request from 'superagent';
 import {getTransHost} from './config.js';
 
 const app = express();
 
-app.use(bodyParser.json());
+// Allow requests from client
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/', function(req, res) {
+// Get Bus information from TransLink api as json
+app.get("/busses/", function(req, response) {
   request
     .get(getTransHost())
     .accept('application/json')
     .type('application/json')
     .end((err, res) => {
-    		response.send('finsihed');
-        console.log(res.body);
+      console.log('GET busses');
+      if (err) {
+        console.log('GET busses error');
+        response.send(500).send("Broken");
+      }
+      response.send(res.body);
     });
 });
 
