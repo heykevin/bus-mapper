d5837b209574ecc8f4d51d4b709396d9b27e5080
Kevin Tang <tkevin@ualberta.ca>
2017-11-27 15:24:45 -0700

    Set coor to UBC also wtf superagent

--- service/index.js
+++ service/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import request from 'superagent';
+import axios from 'axios';
 import {getTransHost} from './config.js';
 
 const app = express();
@@ -13,20 +14,37 @@ app.use(function(req, res, next) {
 
 // Get Bus information from TransLink api as json
 app.get("/busses/", function(req, response) {
-  request
-    .get(getTransHost())
-    .accept('application/json')
-    .type('application/json')
-    .end((err, res) => {
-      console.log('GET busses');
-      if (err) {
-        console.log('GET busses error');
-        response.send(500).send("Broken");
-      }
-      response.send(res.body);
-    });
-});
+  // request
+  //   .get(getTransHost())
+  //   .accept('application/json')
+  //   .type('application/json')
+  //   .end((err, res) => {
+  //     console.log('GET busses');
+  //     if (err) {
+  //       console.log('GET busses error');
+  //       // response.send(500).send("Broken");
+  //     }
+  //     // console.log(res.body);
+  //     console.log(getTransHost());
+  //     console.log('superagent');
+  //     console.log(res.body[0]);
+  //     console.log(res.status);
+  //     console.log(res.statusType);
 
+  //     // response.send(res.body);
+  //   });
+
+  axios.get(getTransHost(), {headers: {Accept: 'application/json', Type:'application/json'}})
+    .then(function (res) {
+      response.send(res.data);
+      console.log('axios');
+    })
+    .catch(function (error) {
+      console.log(error);
+      response.send(500).send("broken");
+    });
+  });
+  
 app.listen(
   5000,
   () => console.log(`Listening on localhost:5000`)
